0929c4aaec3b20528fab95ac9baac0f3f5f20622
Artur642 <136634672+Artur642@users.noreply.github.com>
2024-05-08 16:15:44 +0200

    ok

--- index.js
+++ index.js
@@ -1,47 +1,61 @@
-//1
-const age = 13;
-console.log(age);
+// //1
+// const age = 13;
+// console.log(age);
 
-//2
-const myUser ='Artur';
-console.log(myUser);
+// //2
+// const myUser ='Artur';
+// console.log(myUser);
 
-//3
-const isStudent = true;
-console.log(isStudent);
+// //3
+// const isStudent = true;
+// console.log(isStudent);
 
-//4
-const myString = "цитата";
-console.log(myString)
+// //4
+// const myString = "цитата";
+// console.log(myString)
 
-//5
-const myNumber = 4;
-const a = myNumber + 10
-console.log(a);
+// //5
+// const myNumber = 4;
+// const a = myNumber + 10
+// console.log(a);
 
-//6
-const myNull = null;
-console.log(myNull)
+// //6
+// const myNull = null;
+// console.log(myNull)
 
-//7
-const user = prompt("Вкажіть ваше ім'я");
-alert('Вітаю вас, ${user}')
+// //7
+// const user = prompt("Вкажіть ваше ім'я");
+// alert('Вітаю вас, ${user}')
 
-//8
-const userName = confirm('Чи приймаєте умову?')
-if (userName === true) {
-    alert("Дякую за підтвердження")
-}
-else{
-    alert("Дію відмінено")
-}
+// //8
+// const userName = confirm('Чи приймаєте умову?')
+// if (userName === true) {
+//     alert("Дякую за підтвердження")
+// }
+// else{
+//     alert("Дію відмінено")
+// }
 
-//9
-alert("небезпечна Дія")
-const danger = confirm('Дія небезпечна, ви точно підверджуєте?')
-if (danger === true) {
-    alert("Дякую за підтвердження")
-}
-else{
-    alert("Дію відмінено")
-}
+// //9
+// alert("небезпечна Дія")
+// const danger = confirm('Дія небезпечна, ви точно підверджуєте?')
+// if (danger === true) {
+//     alert("Дякую за підтвердження")
+// }
+// else{
+//     alert("Дію відмінено")
+// }
+result= 5 + 5 + '5';
+console.log(result)
+
+emeil = 'freshshow9999@gmail.com';
+console.log(emeil.includes('freshshow9999@gmail.com'))
+console.log(emeil.includes('@'))
+
+const fullName = 'My' + ' ' + 'name' + ' ' + 'is' + ' ' + 'Viktor' ;
+console.log(fullName)
+
+const userName = "Олександро";
+const payment = 300;
+const message = `Дякуємо, ${userName}! До сплати ${payment} гривень`;
+alert(message);
