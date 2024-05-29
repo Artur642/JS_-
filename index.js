0951236be72df2d3fb3e2761356030541b0365ef
Artur642 <136634672+Artur642@users.noreply.github.com>
2024-05-29 17:00:34 +0200

    ок

--- index.js
+++ index.js
@@ -95,12 +95,52 @@
         
 // }
 
-function checkNumder() {
-    const numberInput = parseFloat(document.getElementById("numberInput").value);
+// function checkNumder() {
+//     const numberInput = parseFloat(document.getElementById("numberInput").value);
 
-    if(!isNaN(numberInput) && numberInput > 10 && numberInput < 20) {
-        alert( "Число входить в діапазон від 10 до 20")
-    } else {
-        alert("Число не входить в діапазон від 10 до 20")
+//     if(!isNaN(numberInput) && numberInput > 10 && numberInput < 20) {
+//         alert( "Число входить в діапазон від 10 до 20")
+//     } else {
+//         alert("Число не входить в діапазон від 10 до 20")
+//     }
+// }
+
+// 1
+// let i = 1;
+// while (i <= 10){
+//     console.log(i);
+//     i++
+// }
+
+// 2
+// for (let i = 2; i <= 20; i++){
+//     if (i % 2 === 0) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// 3
+// for(let i = 1; i <= 10; i++){
+//     let result = 7 * i; 
+//     console.log(`7 * ${i} = ${result}`)
+// }
+
+// 6
+// let n = 7;
+// for (let i = 1; i < n; i++){
+//     if (i > n) {
+//         break
+//     }
+//     console.log(i);
+// }
+
+// 7
+let i = 1;
+while( i <= 20) {
+    if (i % 3 === 0) {
+        i++;
+        continue;
     }
+    console.log(i)
 }
