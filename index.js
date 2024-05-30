87f1625db0fdd4ec2b4d8c8465855b5a7de44442
Artur642 <136634672+Artur642@users.noreply.github.com>
2024-05-30 20:02:17 +0200

    ok

--- index.js
+++ index.js
@@ -136,11 +136,110 @@
 // }
 
 // 7
-let i = 1;
-while( i <= 20) {
-    if (i % 3 === 0) {
-        i++;
-        continue;
+// let i = 1;
+// while( i <= 20) {
+//     if (i % 3 === 0) {
+//         i++;
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+
+// 1
+function showChoise() {
+    const select = document.getElementById("drinks");
+    const selected = select.options[select.selectedIndex].value;
+
+    if (selected === "coffee") {
+        alert("Ви вибрали каву")
+    } else if (selected === "tea") {
+        alert("Ви вибрали чай");
+    } else if (selected === "juice") {
+        alert("Ви вибрали сік");
     }
-    console.log(i)
+}
+
+// 2
+
+function checkDay() {
+    const inputDay = document.getElementById("inputDay").value;
+
+    if (
+        inputDay === "понеділок" ||
+        inputDay === "вівторок" ||
+        inputDay === "середа" ||
+        inputDay === "четаверт" ||
+        inputDay === "п'ятниця" 
+    ) {
+        alert("Зараз будні дні")
+    } else if (inputDay === "субота" || inputDay === "неділя") {
+        alert("Зараз вихідні")
+    } else {
+        alert("You have problem")
+    }
+}
+
+// 3
+
+function checkMonth() {
+    const inputMonth = document.getElementById("inputMonth").value;
+
+    if (inputMonth == 1 || inputMonth == 2 || inputMonth == 12) {
+        alert("It`s winter!")
+    } else if (inputMonth == 3 || inputMonth == 4 || inputMonth == 5){
+        alert("It`s spring!");
+    } else if (inputMonth == 6 || inputMonth == 7 || inputMonth == 8){
+        alert("It`s summer!");
+    } else if (inputMonth == 9 || inputMonth == 10 || inputMonth == 11){
+        alert("It`s autumn!")
+    } else {
+        alert("You have problem")
+    }
+        
+}
+
+// 4
+
+function inputMonthDay() {
+    const inputMonthDay = document.getElementById("inputMonthDay").value;
+
+    if (
+        inputMonthDay == 1 ||
+        inputMonthDay == 3 ||
+        inputMonthDay == 5 ||
+        inputMonthDay == 7 ||
+        inputMonthDay == 8 ||
+        inputMonthDay == 10 ||
+        inputMonthDay == 12
+    ) {
+        alert("In this month 31 days");
+    } else if (
+        inputMonthDay == 4 ||
+        inputMonthDay == 6 ||
+        inputMonthDay == 9 ||
+        inputMonthDay == 11
+    ) {
+        alert("In this month 30 days");
+    } else if (inputMonthDay == 2) {
+        alert("In this month 28 or 29 days")
+    } else {
+        alert("You have problem")
+    }
+}
+
+// 5
+
+function checkColor() {
+    const inputColor = document.getElementById("inputColor").value;
+
+    if (inputColor === "червоний"){
+        alert("стоп");
+    } else if (inputColor === "жовтий") {
+        alert("чекати");
+    } else if (inputColor === "зелений") {
+        alert("йти");
+    } else {
+        alert("You have problem")
+    }
 }
