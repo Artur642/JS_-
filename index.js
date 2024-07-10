18ec5d3976e4e4d67c959ef2d118583888eeac09
Artur642 <136634672+Artur642@users.noreply.github.com>
2024-07-10 13:19:21 +0200

    ok

--- index.js
+++ index.js
@@ -520,76 +520,229 @@
 
 
 //1
-const bankAccount = {
-    ownerName: 'Artur',
-    accountNumber: 0,
-    balance: 5000,
-    deposit(amount) {
-        this.balance += amount;
-        updateBalance();
-        alert('Потояний баланс' + this.balance);
-    },
-    withdraw(amount) {
-        if (amount > this.balance) {
-            alert('Мало коштів');
-            return;
-        } else {
-            this.balance -= amount;
-            updateBalance();
-            alert('Поточний баланс:' + this.balance);
+// const bankAccount = {
+//     ownerName: 'Artur',
+//     accountNumber: 0,
+//     balance: 5000,
+//     deposit(amount) {
+//         this.balance += amount;
+//         updateBalance();
+//         alert('Потояний баланс' + this.balance);
+//     },
+//     withdraw(amount) {
+//         if (amount > this.balance) {
+//             alert('Мало коштів');
+//             return;
+//         } else {
+//             this.balance -= amount;
+//             updateBalance();
+//             alert('Поточний баланс:' + this.balance);
+//         }
+//     },
+//     displayInfo() {
+//         document.getElementById('balance').innerText = `${this.balance} грн.`;
+//     }
+// };
+// bankAccount.displayInfo();
+// function updateBalance() {
+//     document.getElementById('balance').textContent = bankAccount.balance;
+// };
+// function deposit() {
+//     let amount = prompt("Вкажіть суму:");
+//     if (amount > 0 && !isNaN(amount)) {
+//         bankAccount.deposit(parseFloat(amount));
+//     } else {
+//         alert('Невірні дані');
+//     }
+// };
+// function withdraw() {
+//     let amount = prompt("Вкажіть суму:");
+//     if (amount > 0 && !isNaN(amount)) {
+//         bankAccount.deposit(parseFloat(amount));
+//     } else {
+//         alert('Невірні дані');
+//     }
+// };
+
+
+// //4
+// const movie = {
+//     title: 'Hello',
+//     director: 'Tom',
+//     year: 1999,
+//     rating: 9,
+//     displayInfo() {
+//         document.getElementById('title').innerText = `Tirle: ${this.title}`;
+//         document.getElementById('director').innerText = `Director: ${this.title}`;
+//         document.getElementById('year').innerText = `Year: ${this.title}`;
+//         document.getElementById('rating').innerText = `Rating: ${this.title}`;
+//     },
+// };
+// function updateRating() {
+//     const ratingNew = parseFloat(document.getElementById('change').value);
+//     if (ratingNew > 8 && !isNaN(ratingNew)) {
+//         movie.rating = ratingNew;
+//         movie.displayInfo();
+//         const title = document.getElementById('title');
+//         title.style.color = 'green';
+//     } else if (isNaN(ratingNew)) {
+//         alert('ВВедіть число');
+//     } else {
+//         movie.rating = ratingNew;
+//         movie.displayInfo();
+//     }
+// };
+// movie.displayInfo();
+
+
+//1
+const user = {
+    name: 'Mango',
+    age: 20,
+    hobby: 'html',
+    premium: true,
+};
+user.mood = 'happy';
+user.hobby = 'skydiving';
+user.premium = false;
+
+const keys = Object.keys(user);
+for (const key of keys) {
+    console.log(`${key}:${user[key]}`);
+}
+
+//2
+const countProps = function (obj) {
+    return Object.keys(obj).length;
+};
+
+
+console.log(countProps({}));// 0
+
+console.log(countProps({ name: 'Mango', age: 2 }));// 2
+
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }));// 3
+
+
+// 3
+const findBestEmployee = function (employees) {
+    let BestEmployee = '';
+    let maxTask = 0;
+
+    for(const [name, tasks] of Object.entries(employees)) {
+        if(tasks > maxTask) {
+            maxTask = tasks;
+            BestEmployee = name;
         }
-    },
-    displayInfo() {
-        document.getElementById('balance').innerText = `${this.balance} грн.`;
     }
-};
-bankAccount.displayInfo();
-function updateBalance() {
-    document.getElementById('balance').textContent = bankAccount.balance;
-};
-function deposit() {
-    let amount = prompt("Вкажіть суму:");
-    if (amount > 0 && !isNaN(amount)) {
-        bankAccount.deposit(parseFloat(amount));
-    } else {
-        alert('Невірні дані');
-    }
-};
-function withdraw() {
-    let amount = prompt("Вкажіть суму:");
-    if (amount > 0 && !isNaN(amount)) {
-        bankAccount.deposit(parseFloat(amount));
-    } else {
-        alert('Невірні дані');
-    }
-};
+    return BestEmployee;
+  };
+  
+ 
+  console.log(
+    findBestEmployee({
+      ann: 29,
+      david: 35,
+      helen: 1,
+      lorence: 99,
+    }),
+  ); // lorence
+  
+  console.log(
+    findBestEmployee({
+      poly: 12,
+      mango: 17,
+      ajax: 4,
+    }),
+  ); // mango
+  
+  console.log(
+    findBestEmployee({
+      lux: 147,
+      david: 21,
+      kiwi: 19,
+      chelsy: 38,
+    }),
+  ); // lux
 
 
-//4
-const movie = {
-    title: 'Hello',
-    director: 'Tom',
-    year: 1999,
-    rating: 9,
-    displayInfo() {
-        document.getElementById('title').innerText = `Tirle: ${this.title}`;
-        document.getElementById('director').innerText = `Director: ${this.title}`;
-        document.getElementById('year').innerText = `Year: ${this.title}`;
-        document.getElementById('rating').innerText = `Rating: ${this.title}`;
-    },
-};
-function updateRating() {
-    const ratingNew = parseFloat(document.getElementById('change').value);
-    if (ratingNew > 8 && !isNaN(ratingNew)) {
-        movie.rating = ratingNew;
-        movie.displayInfo();
-        const title = document.getElementById('title');
-        title.style.color = 'green';
-    } else if (isNaN(ratingNew)) {
-        alert('ВВедіть число');
-    } else {
-        movie.rating = ratingNew;
-        movie.displayInfo();
+// 4
+const countTotalSalary = function (employees) {
+    let totalSalary = 0;
+    
+    for(const salary of Object.values(employees)) {
+        totalSalary += salary;
     }
-};
-movie.displayInfo();
+    return totalSalary
+
+  };
+  
+  
+  console.log(countTotalSalary({})); // 0
+  
+  console.log(
+    countTotalSalary({
+      mango: 100,
+      poly: 150,
+      alfred: 80,
+    }),
+  ); // 330
+  
+  console.log(
+    countTotalSalary({
+      kiwi: 200,
+      lux: 50,
+      chelsy: 150,
+    }),
+  ); // 400
+
+
+  // 5
+//   const products = [
+//     { name: 'Радар', price: 1300, quantity: 4 },
+//     { name: 'Сканер', price: 2700, quantity: 3 },
+//     { name: 'Дроїд', price: 400, quantity: 7 },
+//     { name: 'Захоплення', price: 1200, quantity: 2 },
+//   ];
+  
+//   const getAllPropValues = function (arr, prop) {
+//     const values = [];
+//     for (const item of arr) {
+//         if(item.hasOwnProperty(prop)) {
+//             values.push(item[prop]);
+//         }
+//     }
+//     return values
+//   };
+  
+//   /*
+//    * Викличи функції для перевірки працездатності твоєї реалізації.
+//    */
+//   console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроїд', 'Захоплення']
+  
+//   console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
+  
+//   console.log(getAllPropValues(products, 'category')); // []
+
+
+// 6
+const products2 = [
+    { name: 'Радар', price: 1300, quantity: 4 },
+    { name: 'Сканер', price: 2700, quantity: 3 },
+    { name: 'Дроїд', price: 400, quantity: 7 },
+    { name: 'Захоплення', price: 1200, quantity: 2 },
+  ];
+  
+  const calculateTotalPrice = function (allProdcuts, productName) {
+    for (const product of allProdcuts) {
+        if (product.name === productName) {
+            return product.price * product.quantity;
+        }
+     }
+     return 0;
+  };
+  
+  
+  console.log(calculateTotalPrice(products2, 'Радар')); // 5200
+  
+  console.log(calculateTotalPrice(products2, 'Дроїд')); // 2800
