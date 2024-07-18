82dca960dce55e77838395ec78d0a10eea87ddea
Artur642 <136634672+Artur642@users.noreply.github.com>
2024-07-18 18:53:36 +0200

    ok

--- index.js
+++ index.js
@@ -596,115 +596,115 @@
 
 
 //1
-const user = {
-    name: 'Mango',
-    age: 20,
-    hobby: 'html',
-    premium: true,
-};
-user.mood = 'happy';
-user.hobby = 'skydiving';
-user.premium = false;
+// const user = {
+//   name: 'Mango',
+//   age: 20,
+//   hobby: 'html',
+//   premium: true,
+// };
+// user.mood = 'happy';
+// user.hobby = 'skydiving';
+// user.premium = false;
 
-const keys = Object.keys(user);
-for (const key of keys) {
-    console.log(`${key}:${user[key]}`);
-}
+// const keys = Object.keys(user);
+// for (const key of keys) {
+//   console.log(`${key}:${user[key]}`);
+// }
 
-//2
-const countProps = function (obj) {
-    return Object.keys(obj).length;
-};
-
-
-console.log(countProps({}));// 0
-
-console.log(countProps({ name: 'Mango', age: 2 }));// 2
-
-console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }));// 3
+// //2
+// const countProps = function (obj) {
+//   return Object.keys(obj).length;
+// };
 
 
-// 3
-const findBestEmployee = function (employees) {
-    let BestEmployee = '';
-    let maxTask = 0;
+// console.log(countProps({}));// 0
 
-    for(const [name, tasks] of Object.entries(employees)) {
-        if(tasks > maxTask) {
-            maxTask = tasks;
-            BestEmployee = name;
-        }
-    }
-    return BestEmployee;
-  };
-  
- 
-  console.log(
-    findBestEmployee({
-      ann: 29,
-      david: 35,
-      helen: 1,
-      lorence: 99,
-    }),
-  ); // lorence
-  
-  console.log(
-    findBestEmployee({
-      poly: 12,
-      mango: 17,
-      ajax: 4,
-    }),
-  ); // mango
-  
-  console.log(
-    findBestEmployee({
-      lux: 147,
-      david: 21,
-      kiwi: 19,
-      chelsy: 38,
-    }),
-  ); // lux
+// console.log(countProps({ name: 'Mango', age: 2 }));// 2
+
+// console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }));// 3
 
 
-// 4
-const countTotalSalary = function (employees) {
-    let totalSalary = 0;
-    
-    for(const salary of Object.values(employees)) {
-        totalSalary += salary;
-    }
-    return totalSalary
+// // 3
+// const findBestEmployee = function (employees) {
+//   let BestEmployee = '';
+//   let maxTask = 0;
 
-  };
-  
-  
-  console.log(countTotalSalary({})); // 0
-  
-  console.log(
-    countTotalSalary({
-      mango: 100,
-      poly: 150,
-      alfred: 80,
-    }),
-  ); // 330
-  
-  console.log(
-    countTotalSalary({
-      kiwi: 200,
-      lux: 50,
-      chelsy: 150,
-    }),
-  ); // 400
+//   for (const [name, tasks] of Object.entries(employees)) {
+//     if (tasks > maxTask) {
+//       maxTask = tasks;
+//       BestEmployee = name;
+//     }
+//   }
+//   return BestEmployee;
+// };
 
 
-  // 5
+// console.log(
+//   findBestEmployee({
+//     ann: 29,
+//     david: 35,
+//     helen: 1,
+//     lorence: 99,
+//   }),
+// ); // lorence
+
+// console.log(
+//   findBestEmployee({
+//     poly: 12,
+//     mango: 17,
+//     ajax: 4,
+//   }),
+// ); // mango
+
+// console.log(
+//   findBestEmployee({
+//     lux: 147,
+//     david: 21,
+//     kiwi: 19,
+//     chelsy: 38,
+//   }),
+// ); // lux
+
+
+// // 4
+// const countTotalSalary = function (employees) {
+//   let totalSalary = 0;
+
+//   for (const salary of Object.values(employees)) {
+//     totalSalary += salary;
+//   }
+//   return totalSalary
+
+// };
+
+
+// console.log(countTotalSalary({})); // 0
+
+// console.log(
+//   countTotalSalary({
+//     mango: 100,
+//     poly: 150,
+//     alfred: 80,
+//   }),
+// ); // 330
+
+// console.log(
+//   countTotalSalary({
+//     kiwi: 200,
+//     lux: 50,
+//     chelsy: 150,
+//   }),
+// ); // 400
+
+
+// 5
 //   const products = [
 //     { name: 'Радар', price: 1300, quantity: 4 },
 //     { name: 'Сканер', price: 2700, quantity: 3 },
 //     { name: 'Дроїд', price: 400, quantity: 7 },
 //     { name: 'Захоплення', price: 1200, quantity: 2 },
 //   ];
-  
+
 //   const getAllPropValues = function (arr, prop) {
 //     const values = [];
 //     for (const item of arr) {
@@ -714,35 +714,162 @@ const countTotalSalary = function (employees) {
 //     }
 //     return values
 //   };
-  
+
 //   /*
 //    * Викличи функції для перевірки працездатності твоєї реалізації.
 //    */
 //   console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроїд', 'Захоплення']
-  
+
 //   console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
-  
+
 //   console.log(getAllPropValues(products, 'category')); // []
 
 
 // 6
-const products2 = [
-    { name: 'Радар', price: 1300, quantity: 4 },
-    { name: 'Сканер', price: 2700, quantity: 3 },
-    { name: 'Дроїд', price: 400, quantity: 7 },
-    { name: 'Захоплення', price: 1200, quantity: 2 },
-  ];
-  
-  const calculateTotalPrice = function (allProdcuts, productName) {
-    for (const product of allProdcuts) {
-        if (product.name === productName) {
-            return product.price * product.quantity;
-        }
-     }
-     return 0;
-  };
-  
-  
-  console.log(calculateTotalPrice(products2, 'Радар')); // 5200
-  
-  console.log(calculateTotalPrice(products2, 'Дроїд')); // 2800
+// const products2 = [
+//     { name: 'Радар', price: 1300, quantity: 4 },
+//     { name: 'Сканер', price: 2700, quantity: 3 },
+//     { name: 'Дроїд', price: 400, quantity: 7 },
+//     { name: 'Захоплення', price: 1200, quantity: 2 },
+//   ];
+
+//   const calculateTotalPrice = function (allProdcuts, productName) {
+//     for (const product of allProdcuts) {
+//         if (product.name === productName) {
+//             return product.price * product.quantity;
+//         }
+//      }
+//      return 0;
+//   };
+
+
+//   console.log(calculateTotalPrice(products2, 'Радар')); // 5200
+
+//   console.log(calculateTotalPrice(products2, 'Дроїд')); // 2800
+
+
+
+/*
+ * Типів транзацкій всього два.
+ * Можна покласти або зняти гроші з рахунку.
+ */
+const Transaction = {
+  DEPOSIT: 'deposit',
+  WITHDRAW: 'withdraw',
+};
+
+/*
+ * Кожна транзакція - це об'єкт з властивостями: id, type і amount
+ */
+
+const account = {
+  // Поточний баланс рахунку
+  balance: 0,
+
+  // Історія транзакцій
+  transactions: [],
+
+  /*
+   * Метод створює і повертає об'єкт транзакції.
+   * Приймає суму і тип транзакції.
+   */
+  createTransaction(amount, type) {
+    return {
+      id: this.transactions.length + 1,
+      type,
+      amount,
+    }
+  },
+
+  /*
+   * Метод відповідає за додавання суми до балансу.
+   * Приймає суму танзакції.
+   * Викликає createTransaction для створення об'єкта транзакції
+   * після чого додає його в історію транзакцій
+   */
+  deposit(amount) {
+    const transactions = this.createTransaction(amount, Transaction.DEPOSIT);
+    this.transactions.push(transactions);
+    this.balance += amount;
+  },
+
+  /*
+   * Метод відповідає за зняття суми з балансу.
+   * Приймає суму танзакції.
+   * Викликає createTransaction для створення об'єкта транзакції
+   * після чого додає його в історію транзакцій.
+   *
+   * Якщо amount більше, ніж поточний баланс, виводь повідомлення
+   * про те, що зняття такої суми не можливо, недостатньо коштів.
+   */
+  withdraw(amount) { 
+    if(amount > this.balance) {
+      console.log("Зняття такої суми неможливе, недостатньо коштів");
+      return;
+    }
+    const transactions = this.createTransaction(amount, Transaction.WITHDRAW);
+    this.transactions.push(transactions);
+    this.balance -= amount;
+  },
+
+  /*
+   * Метод повертає поточний баланс
+   */
+  getBalance() { 
+    return this.balance;
+  },
+
+  /*
+   * Метод шукає і повертає об'єкт транзакції по id
+   */
+  getTransactionDetails(id) { 
+    return this.transactions.find(transaction => transaction.id === id)
+  },
+
+  /*
+   * Метод повертає кількість коштів
+   * певного типу транзакції з усієї історії транзакцій
+   */
+  getTransactionTotal(type) { 
+    return this.transactions
+      .filter(transaction => transaction.type === type)
+      .reduce((total, transaction) => total + transaction.amount, 0);
+  },
+};
+account.deposit(100);
+account.withdraw(50);
+account.deposit(200);
+console.log(account.getBalance());
+
+
+//1
+const user = {
+  name: 'Mango',
+  age: 20,
+  hobby: 'html',
+  premium: true,
+};
+
+const { name, age, hobby, premium } = user;
+
+console.log(name);
+console.log(age);
+console.log(hobby);
+console.log(premium);
+
+//5
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроїд', price: 400, quantity: 7 },
+  { name: 'Захоплення', price: 1200, quantity: 2 },
+];
+
+  const [product1, product2, product3] = products;
+  const {name:name1, price:price1, quantity:quantity1} = product1;
+  const {name:name2, price:price2, quantity:quantity2} = product2;
+  const {name:name3, price:price3, quantity:quantity3} = product3;
+
+  console.log(name1, price1, quantity1)
+  console.log(name2, price2, quantity2)
+  console.log(name3, price3, quantity3)
