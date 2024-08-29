a838854cbeebdf20ad5ed98e84a4a8ec169d296a
Artur642 <136634672+Artur642@users.noreply.github.com>
2024-08-29 13:57:59 +0200

    okey

--- index.js
+++ index.js
@@ -1563,5 +1563,63 @@
 // img.src = 'cat.jpg';
 
 // task 4
-const listElement = document.getElementById('numberOne');
-listElement.textContent = 'Scss'
+// const listElement = document.getElementById('numberOne');
+// listElement.textContent = 'Scss'
+
+// 18
+// task 1
+// const categoriesList = document.querySelector('#categories');
+
+// const items = categoriesList.querySelectorAll('.item');
+
+// console.log(`У списку ${items.length} категорії.`);
+
+// items.forEach(item => {
+//   const title = item.querySelector('h2').textContent;
+//   const elementsCount = item.querySelectorAll('ul li').length;
+
+//   console.log(`Категорія: ${title}`);
+//   console.log(`Кількість елементів: ${elementsCount}`);
+// });
+// task 2
+// const ingredients = [
+//     'Картопля',
+//     'Гриби',
+//     'Часник',
+//     'Помідори',
+//     'Зелень',
+//     'Приправи',
+// ];
+
+// const ingredientsList = document.getElementById('ingredients');
+// const items = ingredients.map(ingredient => {
+// const li = document.createElement('li');
+// li.textContent = ingredient;
+// return li;
+// });
+// ingredientsList.append(...items);
+// task 3
+const images = [
+    {
+     url:
+      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+     alt: 'White and Black Long Fur Cat',
+    },
+    {
+     url:
+      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    },
+    {
+     url:
+      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+     alt: 'Group of Horses Running',
+    },
+   ];
+   const gallery = document.getElementById('gallery');
+
+   const galleryItems = images.map(image => 
+       `<li><img src="${image.url}" alt="${image.alt}"></li>`
+   ).join('');
+
+   gallery.insertAdjacentHTML('beforeend', galleryItems);
